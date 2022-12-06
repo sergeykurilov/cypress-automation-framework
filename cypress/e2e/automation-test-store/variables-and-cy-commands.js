949bc8a09781a2bc888443b04eb64120cfe22d76
sergeykurilov <kurilovsergey15@gmail.com>
2022-12-06 21:14:15 +0200

    Iterating through Elements - Documentation & Preparation

--- cypress/e2e/automation-test-store/variables-and-cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -34,5 +34,30 @@ describe('Verifying variables, cypress commands and jquery commands', () => {
             cy.log("Found header text: " + headerText);
             expect(headerText).is.eq("Makeup");
         })
+
+
+    });
+
+    it.only('Validates the properties of contact us page',  () => {
+        cy.visit('https://automationteststore.com/index.php?rt=content/contact');
+
+        // Uses cypress commands and chaining
+        cy
+            .contains('form#ContactUsFrm', 'Contact Us Form')
+            .find('#field_11')
+            .should('contain.text', 'First name');
+
+        // JQuery Approach
+        cy.contains('#ContactUsFrm', 'Contact Us Form')
+            .then(text => {
+                const firstNameText = text.find('#field_11').text()
+                expect(firstNameText).to.contain('First')
+
+                // Embedded commands (Closure)
+
+                cy.get('#field_11').then(fnText => {
+                    cy.log(fnText.text());
+                })
+            })
     });
 })
